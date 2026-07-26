77c1072848fdd77bd63e04545c581b0f04880184
yyyuriko18 <y2m3h.family@gmail.com>
2026-07-26 21:06:30 +0900

    ４回目の提出
    いつもお世話になっております。今回の質問事項がたくさんあるので、 figmaにコメントを入れさせていただきました。ご確認いただけますと幸いです。

---
+++ portfolio/js/script.js
@@ -0,0 +1,80 @@
+// コンタクト
+// const form = document.querySelector(".contact-page-inner");
+
+// form.addEventListener("submit", function(e){
+//     e.preventDefault();
+
+const form = document.querySelector("form");
+
+if(form){
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+
+        const formData = new FormData();
+
+    formData.append(
+        "entry.2005620554",
+        document.querySelector("#user-name").value
+    );
+
+    formData.append(
+        "entry.494408386",
+        document.querySelector("#user-kana").value
+    );
+
+    formData.append(
+        "entry.1045781291",
+        document.querySelector("#user-mail").value
+    );
+
+    formData.append(
+        "entry.839337160",
+        document.querySelector("#message").value
+    );
+    console.log(formData);
+    
+
+    fetch(
+        "https://docs.google.com/forms/d/e/1FAIpQLSckLEBo74pk5czulVfdnB5BT-Nxqwgm6BjKyvC7yVHX-QQpQw/formResponse",
+        {
+            method: "POST",
+            mode: "no-cors",
+            body: formData
+        }
+    );
+
+    document.body.classList.add("fade-out");
+
+setTimeout(() => {
+    location.href="./thanks.html";
+}, 500);document.body.classList.add("fade-out");
+
+setTimeout(() => {
+    location.href="./thanks.html";
+}, 500);
+    });
+}
+
+
+// ハンバーガー
+$(function(){
+        $(".hamburger").click(function(){
+            $(".hamburger").toggleClass("open");
+            $(".header_nav-sp").fadeToggle();
+        });
+});
+
+// モーダル
+const opens = document.querySelectorAll(".open-button");
+const dialogs = document.querySelectorAll(".dialog");
+const closes = document.querySelectorAll(".close-button");
+
+opens.forEach((open, index) => {
+  open.addEventListener("click", () => {
+    dialogs[index].showModal();
+  });
+
+  closes[index].addEventListener("click", () => {
+    dialogs[index].close();
+  });
+});
